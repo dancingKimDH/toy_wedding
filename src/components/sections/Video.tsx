592a95ddac637651bec7ba9d5a7da97d79df45d6
dancingKimDH <kimdonghyun0701@gmail.com>
2024-02-13 15:25:21 +0900

    video tag

--- src/components/sections/Video.tsx
+++ src/components/sections/Video.tsx
@@ -1,9 +1,21 @@
-import classNames from 'classnames'
+import classNames from 'classnames/bind'
 import styles from './Video.module.scss'
 import Section from '../shared/Section'
 
 const cx = classNames.bind(styles)
 
 export default function Video() {
-  return <Section>Video</Section>
+  return (
+    <Section className={cx('container')}>
+      <video
+        autoPlay={true}
+        muted={true}
+        loop={true}
+        controls={true}
+        poster="/assets/poster.jpg"
+      >
+        <source src="/assets/main.mp4" type="video/mp4" />
+      </video>
+    </Section>
+  )
 }
