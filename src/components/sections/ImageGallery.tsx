d3b00a25002de134489964b438d2628dea3a557a
dancingKimDH <kimdonghyun0701@gmail.com>
2024-02-14 14:31:00 +0900

    galleryImages

---
+++ src/components/sections/ImageGallery.tsx
@@ -0,0 +1,19 @@
+import classNames from 'classnames/bind'
+import styles from './ImageGallery.module.scss'
+import Section from '../shared/Section'
+
+const cx = classNames.bind(styles)
+
+export default function ImageGallery({ images }: { images: string[] }) {
+  return (
+    <Section title="사진첩">
+      <ul className={cx('wrap-images')}>
+        {images.map((src, index) => (
+          <li key={index} className={cx('wrap-image')}>
+            <img src={src} alt="이미지" />
+          </li>
+        ))}
+      </ul>
+    </Section>
+  )
+}
