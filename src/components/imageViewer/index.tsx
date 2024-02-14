eddf0f7d9a3ec20ad1388e127e4c44763b908585
dancingKimDH <kimdonghyun0701@gmail.com>
2024-02-14 15:32:09 +0900

    ImageViewer update

--- src/components/imageViewer/index.tsx
+++ src/components/imageViewer/index.tsx
@@ -1,15 +1,57 @@
 import classNames from 'classnames/bind'
 import styles from './ImageViewer.module.scss'
 
+import 'swiper/css'
+
+import './swiper.css'
+
 import { Swiper, SwiperSlide } from 'swiper/react'
+
+import { MdCloseFullscreen } from 'react-icons/md'
 
 const cx = classNames.bind(styles)
 
-export default function ImageViewer() {
+function CloseButton({
+  onClose,
+  className,
+}: {
+  onClose: () => void
+  className: string
+}) {
+  return <MdCloseFullscreen className={className} onClick={onClose} />
+}
+
+export default function ImageViewer({
+  images,
+  open = false,
+  selectedIdx,
+  onClose,
+}: {
+  images: string[]
+  open: boolean
+  selectedIdx: number
+  onClose: () => void
+}) {
+  if (open === false) {
+    return null
+  }
+
   return (
     <div className={cx('dimmed')}>
-      <Swiper spaceBetween={20} slidesPerView={1} loop={true} initialSlide={0}>
-        <SwiperSlide />
+      <CloseButton className={cx('icon-close')} onClose={onClose} />
+      <Swiper
+        spaceBetween={20}
+        slidesPerView={1}
+        loop={true}
+        initialSlide={selectedIdx}
+      >
+        {images.map((src, index) => {
+          return (
+            <SwiperSlide key={index}>
+              <img src={src} alt="이미지" />
+            </SwiperSlide>
+          )
+        })}
       </Swiper>
     </div>
   )
--- src/components/sections/ImageGallery.tsx
+++ src/components/sections/ImageGallery.tsx
@@ -3,21 +3,43 @@ import styles from './ImageGallery.module.scss'
 import Section from '../shared/Section'
 import ImageViewer from '../imageViewer'
 
+import { useState } from 'react'
+
 const cx = classNames.bind(styles)
 
 export default function ImageGallery({ images }: { images: string[] }) {
+  const [selectedIdx, setSelectedIdx] = useState(-1)
+  const open = selectedIdx > -1
+
+  const handleSelectedImage = (idx: number) => {
+    setSelectedIdx(idx)
+  }
+
+  const handleClose = () => {
+    setSelectedIdx(-1)
+  }
+
   return (
     <>
       <Section title="사진첩">
         <ul className={cx('wrap-images')}>
           {images.map((src, index) => (
-            <li key={index} className={cx('wrap-image')}>
+            <li
+              key={index}
+              className={cx('wrap-image')}
+              onClick={() => handleSelectedImage(index)}
+            >
               <img src={src} alt="이미지" />
             </li>
           ))}
         </ul>
       </Section>
-      <ImageViewer />
+      <ImageViewer
+        images={images}
+        open={open}
+        selectedIdx={selectedIdx}
+        onClose={handleClose}
+      />
     </>
   )
 }
